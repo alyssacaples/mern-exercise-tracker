f15356ebfec3dccc5e5c05c283f80d5bbba5d917
alyssa caples <alyssacaples@outlook.com>
2021-01-06 15:08:20 -0500

    running into a bunch of strange errors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
     );
 
 const connection = mongoose.connection;
@@ -19,6 +19,13 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter );
+
 app.listen(port, () =>{
     console.log(`Server is running on port: ${port}`);
 });
+
